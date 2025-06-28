8b94693d488e560ac84ebc243e0846479cb07efa
wahengchang <wahengchang2023@gmail.com>
2025-06-28 23:10:42 +0800

    fix template

--- js/pages/Navbar.js
+++ js/pages/Navbar.js
@@ -4,9 +4,8 @@ export default Vue.defineComponent({
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex items-center justify-between h-16">
                     <div class="flex items-center space-x-8">
-                        <h1 class="text-xl font-bold text-white">OpenAI Structure Response Editor</h1>
+                        <a href="/" class="text-xl font-bold text-white">Prompt Maker</a>
                         <div class="flex space-x-8">
-                            <a href="/" class="border-b-2 border-transparent text-gray-400 hover:text-white hover:border-blue-500 px-1 pt-1">Home</a>
                             <a href="/templates.html" class="border-b-2 border-transparent text-gray-400 hover:text-blue-400 hover:border-blue-500 px-1 pt-1">Templates</a>
                         </div>
                     </div>
